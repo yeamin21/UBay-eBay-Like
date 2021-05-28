345f23fe310620057c2c7700ddb820f201841c3f
Yeamin DM <yeamin21@outlook.com>
2021-05-28 18:51:00 +0600

    Step:1

--- user/models.py
+++ user/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
 class User(AbstractUser):
-    email = models.CharField(max_length=250, unique=True)
+    email = models.EmailField(max_length=250, unique=True)
     username = models.CharField(null=True, blank=True, max_length=20)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return f'{self.email}'
