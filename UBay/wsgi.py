1e3de7200ce49bf34a303df849c38f0b6ae37478
Yeamin DM <yeamin21@outlook.com>
2021-05-29 09:31:47 +0600

    Step:5

--- UBay/wsgi.py
+++ UBay/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UBay.settings')
 
-application = get_wsgi_application()
+
+application = Cling(get_wsgi_application())
