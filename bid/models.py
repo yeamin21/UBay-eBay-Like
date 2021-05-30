24bc11462d0a09a55ce5ba370b3dc5c49e24e693
Yeamin DM <yeamin21@outlook.com>
2021-05-30 16:38:39 +0600

    fixed bid

--- bid/models.py
+++ bid/models.py
@@ -9,7 +9,8 @@ class Bid(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     bid_amount = models.DecimalField(max_digits=9, decimal_places=2, null=True)
     bidder = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_created=True, default=timezone.datetime.now())
+    created_at = models.DateTimeField(
+        auto_created=True, default=timezone.datetime.now)
 
     def __str__(self):
         return f'{self.bid_amount} bid on {self.product}'
